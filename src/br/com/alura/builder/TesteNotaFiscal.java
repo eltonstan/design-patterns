573277934cf547212351f187d30f648e45405f0f
Elton Standerski <eltonstand@gmail.com>
2020-05-02 12:09:54 -0300

    Aula 06: Builder

---
+++ src/br/com/alura/builder/TesteNotaFiscal.java
@@ -0,0 +1,18 @@
+package br.com.alura.builder;
+
+public class TesteNotaFiscal {
+
+	public static void main(String[] args) {
+		NotaFiscal nf = new NotaFiscalBuilder().paraEmpresa("Caelum")
+				.comCnpj("123.456.789/0001-10")
+				.comItem(new ItemDaNota("item 1", 100.0))
+				.comItem(new ItemDaNota("item 2", 200.0))
+				.comItem(new ItemDaNota("item 3", 300.0))
+				.comObservacoes("entregar nf pessoalmente").naDataAtual()
+				.constroi();
+		
+		System.out.println(nf.getValorBruto());
+		System.out.println(nf.getImpostos());
+	}
+
+}
